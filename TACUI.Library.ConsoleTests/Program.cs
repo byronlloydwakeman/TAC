9ead46a10ca1fd9c915371002cda1a1d51f750b6
Byron Lloyd-Wakeman <byzstorm1103@gmail.com>
2021-09-11 04:16:18 -0800

    Added Console Application for Testing the UI.Library
    Added a console application to be able to manually test the UI.Library project

---
+++ TACUI.Library.ConsoleTests/Program.cs
@@ -0,0 +1,41 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TACDataManager.Library.Models;
+using TACDataManager.Library.Models.Enums;
+using TACUI.Library.AutoFac;
+using TACUI.Library.Endpoints;
+using TACUI.Library.Models;
+using TACUIController;
+
+namespace TACUI.Library.ConsoleTests
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (ILifetimeScope scope = UIContainerConfig.Configure().BeginLifetimeScope())
+            {
+                var apiHelper = scope.Resolve<IAPIHelper>();
+                await apiHelper.Login("someEmail@gmail.com", "Password123");
+                //apiHelper.Logout();
+
+                var autoClick = scope.Resolve<IAutoClickEndpoint>();
+                var models = autoClick.GetModelsWithGivenBoss(EnumBoss.EyeOfCluthulu);
+                //await autoClick.InsertAutoClickModel(new AutoClickUIModel()
+                //{
+                //    MinHealth = 400,
+                //    MinMana = 200,
+                //    Weapons = new List<Enum>() { EnumWeapons.CopperShortSword },
+                //    Boss = EnumBoss.EyeOfCluthulu,
+                //    SequenceOfMoves = new MoveModel() { Moves = new List<string>() { "A", "D", "SPACE", "LEFT" }, Timing = new List<int>() { 100, 200, 50, 100 } },
+                //    Potions = new List<Enum>() { EnumPotion.IronSkin, EnumPotion.Swiftness }
+                //});
+            }
+  
+        }
+    }
+}
